3394c40623843b85fc70604efdb0e52689be8748
kamal0620040 <neupane0620040@outlook.com>
2024-01-01 00:09:15 +0545

    implement: connected with backend from predicting result

--- frontend/app/(root)/(home)/page.jsx
+++ frontend/app/(root)/(home)/page.jsx
@@ -1,7 +1,118 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Home = () => {
-  return <div className="h1-bold">Hello Next.js</div>;
+  const [news, setNews] = useState("");
+  const [modelType, setModelType] = useState("RNN");
+  const [predictionResult, setPredictionResult] = useState(null);
+
+  const handleTextareaChange = (event) => {
+    setNews(event.target.value);
+  };
+
+  // Function to handle the button click and send the POST request
+  const handleButtonClick = () => {
+    // Prepare the data in the required format
+    const postData = {
+      model: modelType.toLowerCase(),
+      news: news,
+    };
+
+    console.log(postData);
+
+    // Make a POST request to the server
+    fetch("http://127.0.0.1:8000/predict/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data as needed
+        console.log("Prediction result:", data);
+        setPredictionResult(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-start mb-6">
+        <p className="mb-3 font-semibold">Select Mode:</p>
+        <DropdownMenu className="bg-black">
+          <DropdownMenuTrigger className="outline-dashed px-10 rounded-sm">
+            {modelType}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-white ml-14">
+            <DropdownMenuLabel>Model Type</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="hover:bg-red-400 hover:text-white hover:cursor-pointer"
+              onClick={() => {
+                setModelType("RNN");
+              }}
+            >
+              RNN
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="hover:bg-red-400 hover:text-white hover:cursor-pointer"
+              onClick={() => {
+                setModelType("LSTM");
+              }}
+            >
+              LSTM
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="hover:bg-red-400 hover:text-white hover:cursor-pointer"
+              onClick={() => {
+                setModelType("GRU");
+              }}
+            >
+              GRU
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <p className="mb-3 font-semibold">Enter your News:</p>
+      <Textarea
+        className="text-gray-600 p-6"
+        value={news}
+        onChange={handleTextareaChange}
+        placeholder="Type something..."
+      />
+      <div className="w-full flex justify-center">
+        <Button className="mt-4" onClick={handleButtonClick}>
+          Predict
+        </Button>
+      </div>
+      {predictionResult && (
+        <div>
+          The News is:{" "}
+          {predictionResult.predictions[0] > 0.5 ? (
+            <span>Fake</span>
+          ) : (
+            <span>True</span>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- frontend/components/navbar/Navbar.jsx
+++ frontend/components/navbar/Navbar.jsx
@@ -7,7 +7,12 @@ const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
-        <Image src="vercel.svg" width={50} height={50} alt="fake news logo" />
+        <Image
+          src="./assets/fake-news-logo.svg"
+          width={40}
+          height={40}
+          alt="fake news logo"
+        />
         <p className="h2-bold text-dark-100 dark:text-light-900 max-sm:hidden">
           <span className="text-primary-500">Fake</span> News
         </p>
